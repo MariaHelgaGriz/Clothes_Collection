2581fadb84d6b7deb90133bad94a01a1f3df5280
MariaHelgaGriz <mariahelga423@gmail.com>
2023-09-13 00:45:12 +0700

    hello

--- main/urls.py
+++ main/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path
 from main.views import show_main
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('main/', include('main.urls')),
 ]
