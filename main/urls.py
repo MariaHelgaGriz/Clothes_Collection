31a47083005f665e1b63ba1020f15a87b25ccead
MariaHelgaGriz <mariahelga423@gmail.com>
2023-10-10 19:55:17 +0700

    assignment 6

--- main/urls.py
+++ main/urls.py
@@ -1,21 +1,4 @@
-"""
-URL configuration for Clothes_Collection project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
-from main.views import show_main
 from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml
 from main.views import show_main, create_product, show_xml, show_json
@@ -23,12 +6,13 @@ from main.views import show_main, create_product, show_xml, show_json, show_xml_
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_product
+from main.views import delete_product, get_product_json, add_product_ajax
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('create-product', create_product, name='create_product'), path('xml/', show_xml, name='show_xml'), path('json/', show_json, name='show_json'), path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), path('register/', register, name='register'), path('login/', login_user, name='login'), path('logout/', logout_user, name='logout'), path('get-product/', get_product_json, name='get_product_json'),
-    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
+    path('', show_main, name='show_main'), path('create-product', create_product, name='create_product'), path('xml/', show_xml, name='show_xml'), path('json/', show_json, name='show_json'), path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), path('register/', register, name='register'), path('login/', login_user, name='login'), path('logout/', logout_user, name='logout'), path('edit-product/<int:id>', edit_product, name='edit_product'), path('delete/<int:id>', delete_product, name='delete_product'), path('get-product/', get_product_json, name='get_product_json'),
+path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 ]
