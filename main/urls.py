1531dd2339772cddb2a54e03fa86f24d3a165d8f
MariaHelgaGriz <mariahelga423@gmail.com>
2023-10-10 14:31:33 +0700

    assignment 5

--- main/urls.py
+++ main/urls.py
@@ -29,5 +29,6 @@ app_name = 'main'
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'), path('xml/', show_xml, name='show_xml'), path('json/', show_json, name='show_json'), path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), path('register/', register, name='register'), path('login/', login_user, name='login'), path('logout/', logout_user, name='logout')
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), path('register/', register, name='register'), path('login/', login_user, name='login'), path('logout/', logout_user, name='logout'), path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 ]
